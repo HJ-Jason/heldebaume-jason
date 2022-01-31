ba406dceeb0f78820a800651d18c77c92151a6d0
HJ-Jason <jason.heldebaume03@gmail.com>
2022-01-31 09:48:42 +0100

    date

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 echo 'Hello from <a href="https://jrcan.dev.netlib.re/">JrCanDev</a> <br>';
 echo '<img src="https://www.docker.com/sites/default/files/horizontal.png">';
-echo '<p>update : 10 janvier 2022 </p>';
+echo '<p>update : 31 01 2022 </p>';
 echo 'test';
 ?>
 
